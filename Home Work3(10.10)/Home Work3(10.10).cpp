1e73378607668d3a8478fbe196339d27501219da
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-10-08 12:51:34 +0200

    Add files via upload

---
+++ Home Work3(10.10)/Home Work3(10.10).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void degree(int a)
+{
+	if (a < 2)
+	{
+		cout << "no";
+	}
+	if (a == 2)
+	{
+		cout << "yes" << "\n";
+	}
+
+	int b=0;
+
+	if (a > 2)
+	{
+		b++;
+		degree(a / 2);
+	}
+}
+
+int main()
+{
+	int a;
+	cin >> a;
+	degree(a);
+}
